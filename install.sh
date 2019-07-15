1907fe2dca2f344c46585427d658b28770921b20
Simple Salah <dev@simplesalah.com>
2019-07-15 05:44:42 -0700

    install script updates

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 
 #Auto-launch Chromium to iqamah page on boot
 mkdir -p ~/.config/autostart
-cp iqamah-times-display.desktop ~/.config/autostart/.
+echo '[Desktop Entry]' > ~/.config/autostart/iqamah-times-display.desktop
+echo 'Name=IqamahTimesDisplay' >> ~/.config/autostart/iqamah-times-display.desktop
+echo 'Type=Application' >> ~/.config/autostart/iqamah-times-display.desktop
+echo "Exec=/usr/lib/chromium-browser/chromium-browser --kiosk $HOME/iqamah-times-display/index.html" >> ~/.config/autostart/iqamah-times-display.desktop
 
 #Disable screen blanking
-sudo xset s off
-sudo xset -dpms
-sudo xset s noblank
+mkdir -p ~/.config/lxsession/LXDE-pi
+echo '@xset s noblank' > ~/.config/lxsession/LXDE-pi/autostart
+echo '@xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart
+echo '@xset s off' >> ~/.config/lxsession/LXDE-pi/autostart
